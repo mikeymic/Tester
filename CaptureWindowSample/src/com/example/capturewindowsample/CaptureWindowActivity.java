27817670d5eecb948caec6e710342cd3ae0d2c69
takashi <takashi@takashi-PC>
2014-03-28 20:01:34 +0900

    Capture Home window by app

---
+++ CaptureWindowSample/src/com/example/capturewindowsample/CaptureWindowActivity.java
@@ -0,0 +1,22 @@
+package com.example.capturewindowsample;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class CaptureWindowActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_capture_window);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.capture_window, menu);
+		return true;
+	}
+
+}
