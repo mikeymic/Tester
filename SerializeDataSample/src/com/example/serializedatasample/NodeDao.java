0c9269b3ecdf4ef82a752c285e4d5707a021e597
takashi <takashi@takashi-PC>
2014-03-28 19:55:34 +0900

    test

--- SerializeDataSample/src/com/example/serializedatasample/NodeDao.java
+++ SerializeDataSample/src/com/example/serializedatasample/NodeDao.java
@@ -7,34 +7,25 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 
 public class NodeDao {
-	
+
 
 	private static final String TABLE_NAME = DatabaseHelper.TABLE_NAME;
 	private static final String ID = DatabaseHelper.CLM_ID;
-//	private static final String  COORDINATE_X = DatabaseHelper.CLM_X;
-//	private static final String  COORDINATE_Y = DatabaseHelper.CLM_Y;
 	private static final String NODE = DatabaseHelper.CLM_NODE;
 
 	private SQLiteDatabase db;
 
-//	private static final String[] COLUMNS = {ID, COORDINATE_X, COORDINATE_Y};
 	private static final String[] COLUMNS = {ID, NODE};
-	
-	FileManager fiManager = new FileManager(); 
-	
+
+	FileManager fiManager = new FileManager();
+
 	public NodeDao (SQLiteDatabase db) {
 		this.db = db;
 	}
 
 	public long insert(byte[] stream) {
-		
+
 		ContentValues values = new ContentValues();
-		
-//		values.put(COORDINATE_X, node.getX());
-//		values.put(COORDINATE_Y, node.getY());
-//		Log.d("TEST", "COORDINATE X = " + String.valueOf(node.getX()));
-//		Log.d("TEST", "COORDINATE X = " + String.valueOf(node.getY()));
-		
 		values.put(NODE, stream);
 		return db.insert(TABLE_NAME, null, values);
 	}
